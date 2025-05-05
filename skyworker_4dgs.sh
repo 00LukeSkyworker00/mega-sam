1938cf67481d5aa380c43f4ca08c304e91a52f19
skyworker-sinica <luke.yc.yang@gmail.com>
2025-05-05 16:15:06 +0800

    fix sh

--- skyworker_4dgs.sh
+++ skyworker_4dgs.sh
@@ -19,9 +19,10 @@
 #   movi_a_0001
 # )
 
-for i in {63..99}
+for i in {0..60}
 do
-  evalset+=("movi_a_00$i")
+  padded=$(printf "%04d" "$i")
+  evalset+=("movi_a_$padded")
 done
 
 # Main directory
@@ -47,55 +48,56 @@ for seq in ${evalset[@]}; do
   python ano_mask.py --dir $DATA_DIR/$seq
 done
 
-export PYTHONPATH="${PYTHONPATH}:$(pwd)/UniDepth"
+# export CUDA_VISIBLE_DEVICES=2
+# export PYTHONPATH="${PYTHONPATH}:$(pwd)/UniDepth"
 
-for seq in ${evalset[@]}; do
+# for seq in ${evalset[@]}; do
 
-  # Skip if the output already exists
-  if [ -f "outputs/${seq}_droid.npz" ]; then
-    cp outputs/${seq}_droid.npz $DATA_DIR/$seq/${seq}.npz
-    cp outputs_cvd/${seq}_sgd_cvd_hr.npz $DATA_DIR/$seq/${seq}_cvd.npz
-    echo "Skipping $seq"
-    continue
-  fi
+#   # Skip if the output already exists
+#   if [ -f "outputs/${seq}_droid.npz" ]; then
+#     cp outputs/${seq}_droid.npz $DATA_DIR/$seq/${seq}.npz
+#     cp outputs_cvd/${seq}_sgd_cvd_hr.npz $DATA_DIR/$seq/${seq}_cvd.npz
+#     echo "Skipping $seq"
+#     continue
+#   fi
 
-  # Run DepthAnything
-  CUDA_VISIBLE_DEVICES=0 python Depth-Anything/run_videos.py --encoder vitl \
-  --load-from Depth-Anything/checkpoints/depth_anything_vitl14.pth \
-  --img-path $DATA_DIR/$seq/images/seq1 \
-  --outdir Depth-Anything/video_visualization/$seq
+#   # Run DepthAnything
+#   python Depth-Anything/run_videos.py --encoder vitl \
+#   --load-from Depth-Anything/checkpoints/depth_anything_vitl14.pth \
+#   --img-path $DATA_DIR/$seq/images/seq1 \
+#   --outdir Depth-Anything/video_visualization/$seq
 
-  # Run UniDepth
-  CUDA_VISIBLE_DEVICES=0 python UniDepth/scripts/demo_mega-sam.py \
-  --scene-name $seq \
-  --img-path $DATA_DIR/$seq/images/seq1 \
-  --outdir UniDepth/outputs
+#   # Run UniDepth
+#   python UniDepth/scripts/demo_mega-sam.py \
+#   --scene-name $seq \
+#   --img-path $DATA_DIR/$seq/images/seq1 \
+#   --outdir UniDepth/outputs
 
-  # Run MegaSaM
-  CUDA_VISIBLE_DEVICE=0 python camera_tracking_scripts/test_demo.py \
-  --datapath=$DATA_DIR/$seq/images/seq1 \
-  --weights=$CKPT_PATH \
-  --scene_name $seq \
-  --mono_depth_path $(pwd)/Depth-Anything/video_visualization \
-  --metric_depth_path $(pwd)/UniDepth/outputs \
-  --disable_vis $@
+#   # Run MegaSaM
+#   python camera_tracking_scripts/test_demo.py \
+#   --datapath=$DATA_DIR/$seq/images/seq1 \
+#   --weights=$CKPT_PATH \
+#   --scene_name $seq \
+#   --mono_depth_path $(pwd)/Depth-Anything/video_visualization \
+#   --metric_depth_path $(pwd)/UniDepth/outputs \
+#   --disable_vis $@
 
-  cp outputs/${seq}_droid.npz $DATA_DIR/$seq/${seq}.npz
+#   cp outputs/${seq}_droid.npz $DATA_DIR/$seq/${seq}.npz
 
-  # Run Raft Optical Flows
-  CUDA_VISIBLE_DEVICES=0 python cvd_opt/preprocess_flow.py \
-  --datapath=$DATA_DIR/$seq/images/seq1 \
-  --model=cvd_opt/raft-things.pth \
-  --scene_name $seq --mixed_precision
+#   # Run Raft Optical Flows
+#   python cvd_opt/preprocess_flow.py \
+#   --datapath=$DATA_DIR/$seq/images/seq1 \
+#   --model=cvd_opt/raft-things.pth \
+#   --scene_name $seq --mixed_precision
 
-  # Run CVD optmization
-  CUDA_VISIBLE_DEVICES=0 python cvd_opt/cvd_opt.py \
-  --scene_name $seq \
-  --w_grad 2.0 --w_normal 5.0
+#   # Run CVD optmization
+#   python cvd_opt/cvd_opt.py \
+#   --scene_name $seq \
+#   --w_grad 2.0 --w_normal 5.0
 
-  cp outputs_cvd/${seq}_sgd_cvd_hr.npz $DATA_DIR/$seq/${seq}_cvd.npz
+#   cp outputs_cvd/${seq}_sgd_cvd_hr.npz $DATA_DIR/$seq/${seq}_cvd.npz
 
-done
+# done
 
 
 
